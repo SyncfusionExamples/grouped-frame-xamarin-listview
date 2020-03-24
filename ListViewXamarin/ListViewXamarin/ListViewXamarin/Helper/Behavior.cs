8c84edd29dfe2beb775caf353a8283ba0f2594e6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-24 17:54:20 +0530

    XAMARIN-22664 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfListView ListView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+
+            ListView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "ContactName",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Contacts);
+                    return item.ContactName[0].ToString();
+                }
+            });
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Converter.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Syncfusion.ListView.XForms;
+using Syncfusion.DataSource.Extensions;
+using Syncfusion.DataSource;
+
+namespace ListViewXamarin
+{
+    #region ThicknessConverter
+    public class ThicknessConverter : IValueConverter
+    {
+        Thickness groupBorderThickness;
+        Thickness lastItemThickness;
+        Thickness defaultThickness;
+        public ThicknessConverter()
+        {
+            groupBorderThickness = new Thickness(1, 1, 1, 0);
+            defaultThickness = new Thickness(1, 0, 1, 0);
+            lastItemThickness = new Thickness(1, 0, 1, 1);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var listView = parameter as SfListView;
+            var itemData = value as Contacts;
+            object key = null;
+            GroupResult actualGroup = null;
+            var descriptor = listView.DataSource.GroupDescriptors[0];
+
+            if (value == null)
+                return defaultThickness;
+
+            if (itemData == null)
+                return groupBorderThickness;
+            else
+            {
+                key = descriptor.KeySelector(itemData);
+                for (int i = 0; i < listView.DataSource.Groups.Count; i++)
+                {
+                    var group = listView.DataSource.Groups[i];
+                    if ((group.Key != null && group.Key.Equals(key)) || group.Key == key)
+                    {
+                        actualGroup = group;
+                        break;
+                    }
+                }
+                var lastItem = actualGroup.GetGroupLastItem();
+
+                if (lastItem == itemData)
+                    return lastItemThickness;
+
+                return defaultThickness;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+    #endregion
+}
